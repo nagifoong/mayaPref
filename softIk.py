b393a595617422d243a2bf14abc8708c03a03e78
nagifoong <38641512+nagifoong@users.noreply.github.com>
2019-11-02 14:53:48 +0800

    Update softIk.py

--- softIk.py
+++ softIk.py
@@ -21,84 +21,98 @@ if ( $d > $D_soft ) {
 
 softJ_end.ty = $new_d;
 """
-[ctrl,ikMain] = [cmds.ls(sl=1)[0],cmds.ls(sl=1,type = "ikHandle")[0]]
-jntList = cmds.ikHandle(ikMain, q=1,jl=1)
-jntList.append(cmds.listRelatives(jntList[-1],type = "joint"))
-ikJnts = [jntList[0],jntList[-1]]
-
-pref = ikMain.split("ikHandle")[0]
-pref = pref[:-1] if pref[-1] == "_" else pref
-total = cmds.xform(jntList[1],q=1,t=1)[0]+cmds.xform(jntList[2],q=1,t=1)[0]
-
-## create node
-softIkJnts = []
-softIkJnts.append(cmds.joint(name = "jnt_%s_softIk1"%pref,p =(0,0,0)))
-softIkJnts.append(cmds.joint(name = "jnt_%s_softIk2"%pref,p =(1,0,0)))
-softIk = cmds.ikHandle(softIkJnts[0],ee=softIkJnts[1],name = "%s_softIkHandle"%pref,shf=0)
-cmds.xform(softIkJnts[0],t=cmds.xform(jntList[0],q=1,ws=1,t=1))
-cmds.xform(softIk[0],t=cmds.xform(ikMain,q=1,ws=1,t=1))
-
-locA = cmds.spaceLocator(name = "%s_locA"%pref)
-locB = cmds.spaceLocator(name = "%s_locB"%pref)
-dimNode = cmds.shadingNode("distanceDimShape",au=1,name = "%s_distanceNodeShape"%pref)
-
-locShp = []
-for ind , loc in enumerate([locA,locB]):
-    cmds.xform(loc,t=cmds.xform(ikJnts[ind],q=1,ws=1,t=1))
-    locShp.append(cmds.listRelatives(loc, type = "locator")[0])
+def softIk(ctrl = "", ikHandle = ""):
+    #[ctrl,ikMain] = [cmds.ls(sl=1)[0],cmds.ls(sl=1,type = "ikHandle")[0]]
+    jntList = cmds.ikHandle(ikMain, q=1,jl=1)
+    jntList.append(cmds.listRelatives(jntList[-1],type = "joint"))
+    ikJnts = [jntList[0],jntList[-1]]
     
-cmds.addAttr(ctrl,ln="softIk",min=0,max=1,k=1,at="double")
-distanceSub = cmds.shadingNode("plusMinusAverage",name = "%s_softIk_distance_SUB",au=1)
-diffSub = cmds.shadingNode("plusMinusAverage",name = "%s_softIk_diff_SUB",au=1)
-negUc = cmds.shadingNode("unitConversion",name = "%s_softIk_diff_NEG",au=1)
-expNode = cmds.shadingNode("multiplyDivide",name = "%s_exp_diff_POW",au=1)
-revSub = cmds.shadingNode("plusMinusAverage",name = "%s_rev_exp_SUB",au=1)
-expMult = cmds.shadingNode("multiplyDivide",name = "%s_softIk_exp_MULT",au=1)
-finalAdd = cmds.shadingNode("plusMinusAverage",name = "%s_softIk_ADD",au=1)
-condNode = cmds.shadingNode("condition",name = "%s_softIk_COND",au=1)
-
-##set attribute##
-cmds.setAttr("%s.input1D[0]"%distanceSub,total)
-cmds.setAttr("%s.operation"%distanceSub,2)
-cmds.setAttr("%s.operation"%diffSub,2)
-cmds.setAttr("%s.conversionFactor"%negUc,-1)
-cmds.setAttr("%s.operation"%expNode,3)
-cmds.setAttr("%s.input1X"%expNode,2.718)
-cmds.setAttr("%s.operation"%revSub,2)
-cmds.setAttr("%s.input1D[0]"%distanceSub,1)
-cmds.setAttr("%s.operation"%expMult,1)
-cmds.setAttr("%s.operation"%finalAdd,1)
-cmds.setAttr("%s.operation"%condNode,2)
-
-##connect attribute##
-cmds.connectAttr("%s.worldPosition"%locShp[0],"%s.startPoint"%dimNode)
-cmds.connectAttr("%s.worldPosition"%locShp[1],"%s.endPoint"%dimNode)
-
-cmds.connectAttr("%s.softIk"%ctrl,"%s.input1D[1]"%distanceSub)
-
-cmds.connectAttr("%s.distance"%dimNode,"%s.input1D[0]"%diffSub)
-cmds.connectAttr("%s.output1D"%distanceSub,"%s.input1D[0]"%diffSub)
-cmds.connectAttr("%s.output1D"%distanceSub,"%s.input1D[1]"%diffSub)
-
-cmds.connectAttr("%s.output1D"%diffSub,"%s.input"%negUc)
-
-cmds.connectAttr("%s.output"%negUc,"%s.input2X"%expNode)
-
-cmds.connectAttr("%s.output"%expNode,"%s.input1D[1]"%revSub)
-
-cmds.connectAttr("%s.softIk"%ctrl,"%s.input1X"%expMult)
-cmds.connectAttr("%s.output1D"%revSub,"%s.input2X"%expMult)
-
-cmds.connectAttr("%s.output1D"%distanceSub,"%s.input1D[0]"%finalAdd)
-cmds.connectAttr("%s.outputX"%expMult,"%s.input1D[1]"%finalAdd)
-
-cmds.connectAttr("%s.output1D"%finalAdd,"%s.colorIfTrueR"%condNode)
-cmds.connectAttr("%s.distance"%dimNode,"%s.colorIfFalseR"%condNode)
-cmds.connectAttr("%s.distance"%dimNode,"%s.firstTerm"%condNode)
-cmds.connectAttr("%s.output1D"%distanceSub,"%s.secondTerm"%condNode)
-
-cmds.connectAttr("%s.outColorR"%condNode,"%s.tx"%softIkJnts[-1])
-
-##clean up
-utilGrp = cmds.group(nmae = "%s_softIk_utilGrp"%pref,em=1)
-cmds.parent(locA,locB,dimNode.replace("Shape",""),utilGrp)
+    pref = ikMain.split("ikHandle")[0]
+    pref = pref[:-1] if pref[-1] == "_" else pref
+    total = cmds.xform(jntList[1],q=1,t=1)[0]+cmds.xform(jntList[2],q=1,t=1)[0]
+    
+    ## create node
+    softIkJnts = []
+    cmds.select(cl=1)
+    softIkJnts.append(cmds.joint(name = "jnt_%s_softIk1"%pref,p =(0,0,0)))
+    softIkJnts.append(cmds.joint(name = "jnt_%s_softIk2"%pref,p =(1,0,0)))
+    softIk = cmds.ikHandle(softIkJnts[0],ee=softIkJnts[1],name = "%s_softIkHandle"%pref,shf=0)
+    cmds.xform(softIkJnts[0],t=cmds.xform(jntList[0],q=1,ws=1,t=1))
+    cmds.xform(softIk[0],t=cmds.xform(ikMain,q=1,ws=1,t=1))
+    
+    locA = cmds.spaceLocator(name = "%s_locA"%pref)
+    locB = cmds.spaceLocator(name = "%s_locB"%pref)
+    dimNode = cmds.shadingNode("distanceDimShape",au=1,name = "%s_distanceNodeShape"%pref)
+    
+    locShp = []
+    for ind , loc in enumerate([locA,locB]):
+        cmds.xform(loc,t=cmds.xform(ikJnts[ind],q=1,ws=1,t=1))
+        locShp.append(cmds.listRelatives(loc, type = "locator")[0])
+        
+    cmds.addAttr(ctrl,ln="softIk",min=0,max=1,k=1,at="double")
+    distanceSub = cmds.shadingNode("plusMinusAverage",name = "%s_softIk_distance_SUB"%pref,au=1)
+    diffSub = cmds.shadingNode("plusMinusAverage",name = "%s_softIk_diff_SUB"%pref,au=1)
+    negUc = cmds.shadingNode("unitConversion",name = "%s_softIk_diff_NEG"%pref,au=1)
+    expNode = cmds.shadingNode("multiplyDivide",name = "%s_exp_diff_POW"%pref,au=1)
+    revSub = cmds.shadingNode("plusMinusAverage",name = "%s_rev_exp_SUB"%pref,au=1)
+    expMult = cmds.shadingNode("multiplyDivide",name = "%s_softIk_exp_MULT"%pref,au=1)
+    finalAdd = cmds.shadingNode("plusMinusAverage",name = "%s_softIk_ADD"%pref,au=1)
+    condNode = cmds.shadingNode("condition",name = "%s_softIk_COND"%pref,au=1)
+    
+    ##set attribute##
+    cmds.setAttr("%s.input1D[0]"%distanceSub,total)
+    cmds.setAttr("%s.operation"%distanceSub,2)
+    cmds.setAttr("%s.operation"%diffSub,2)
+    cmds.setAttr("%s.conversionFactor"%negUc,-1)
+    cmds.setAttr("%s.operation"%expNode,3)
+    cmds.setAttr("%s.input1X"%expNode,2.718)
+    cmds.setAttr("%s.operation"%revSub,2)
+    cmds.setAttr("%s.input1D[0]"%revSub,1)
+    cmds.setAttr("%s.operation"%expMult,1)
+    cmds.setAttr("%s.operation"%finalAdd,1)
+    cmds.setAttr("%s.operation"%condNode,2)
+    
+    ##connect attribute##
+    cmds.connectAttr("%s.worldPosition"%locShp[0],"%s.startPoint"%dimNode)
+    cmds.connectAttr("%s.worldPosition"%locShp[1],"%s.endPoint"%dimNode)
+    
+    cmds.connectAttr("%s.softIk"%ctrl,"%s.input1D[1]"%distanceSub)
+    
+    cmds.connectAttr("%s.distance"%dimNode,"%s.input1D[0]"%diffSub)
+    cmds.connectAttr("%s.output1D"%distanceSub,"%s.input1D[1]"%diffSub)
+    
+    cmds.connectAttr("%s.output1D"%diffSub,"%s.input"%negUc)
+    
+    cmds.connectAttr("%s.output"%negUc,"%s.input2X"%expNode)
+    
+    cmds.connectAttr("%s.outputX"%expNode,"%s.input1D[1]"%revSub)
+    
+    cmds.connectAttr("%s.softIk"%ctrl,"%s.input1X"%expMult)
+    cmds.connectAttr("%s.output1D"%revSub,"%s.input2X"%expMult)
+    
+    cmds.connectAttr("%s.output1D"%distanceSub,"%s.input1D[0]"%finalAdd)
+    cmds.connectAttr("%s.outputX"%expMult,"%s.input1D[1]"%finalAdd)
+    
+    cmds.connectAttr("%s.output1D"%finalAdd,"%s.colorIfTrueR"%condNode)
+    cmds.connectAttr("%s.distance"%dimNode,"%s.colorIfFalseR"%condNode)
+    cmds.connectAttr("%s.distance"%dimNode,"%s.firstTerm"%condNode)
+    cmds.connectAttr("%s.output1D"%distanceSub,"%s.secondTerm"%condNode)
+    
+    cmds.connectAttr("%s.outColorR"%condNode,"%s.tx"%softIkJnts[-1])
+    
+    ##clean up
+    utilGrp = cmds.group(name = "%s_softIk_utilGrp"%pref,em=1)
+    softIkGrp = cmds.group(name = "%s_softIk_grp"%pref,em=1)
+    newIkGrp = cmds.group(name = "%s_ikHandle_result_grp"%pref,em=1)
+    
+    cmds.xform(softIkGrp,t=(cmds.xform(softIk[0],q=1,ws=1,t=1)))
+    cmds.xform(newIkGrp,t=(cmds.xform(ikMain,q=1,ws=1,t=1)))
+    
+    cmds.parent(locB,softIk[0],softIkGrp)
+    cmds.parent(ikMain,newIkGrp)
+    cmds.parent(softIkJnts[0],newIkGrp,softIkGrp,locA,dimNode.replace("Shape",""),utilGrp)
+    
+    cmds.pointConstraint(softIkJnts[-1],newIkGrp,mo=1)
+    cmds.parentConstraint(ctrl,softIkGrp,mo=1)
+    
+    return [newIkGrp,utilGrp]
